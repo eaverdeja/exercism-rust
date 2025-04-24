ad330c92842301295b8c244cf5e02e2d07dc730c
eaverdeja <eaverdeja@gmail.com>
2025-04-24 09:52:22 -0300

    etl

---
+++ etl/src/lib.rs
@@ -0,0 +1,11 @@
+use std::collections::BTreeMap;
+
+pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
+    h.iter().flat_map(|(&k, v)| to_scores(v, k)).collect()
+}
+
+fn to_scores(chars: &[char], score: i32) -> impl Iterator<Item = (char, i32)> {
+    chars
+        .iter()
+        .map(move |&ch| (ch.to_ascii_lowercase(), score))
+}
