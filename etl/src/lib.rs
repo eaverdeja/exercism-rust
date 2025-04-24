7f6a10989fd4366ce46e160a285552c4f096234e
eaverdeja <eaverdeja@gmail.com>
2025-04-24 16:36:29 -0300

    refactoring

--- etl/src/lib.rs
+++ etl/src/lib.rs
@@ -5,7 +5,5 @@ pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
 }
 
 fn to_scores(chars: &[char], score: i32) -> impl Iterator<Item = (char, i32)> {
-    chars
-        .iter()
-        .map(move |&ch| (ch.to_ascii_lowercase(), score))
+    chars.iter().map(move |ch| (ch.to_ascii_lowercase(), score))
 }
