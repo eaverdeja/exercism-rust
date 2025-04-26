243ee76d0d85aa7a13427a66e8c0973368df847e
eaverdeja <eaverdeja@gmail.com>
2025-04-26 10:21:43 -0300

    Update LOC table

--- loc-all.sh
+++ loc-all.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-# runs tokei excluding tests, markdown, TOML files and other assets
-tokei . -e tests/ -e \*.md -e \*.toml -e \*.sh -e \*.svg -f -s code
+# runs `tokei` excluding tests, markdown, TOML files and other assets
+# prints file by file, sorting by lines of code
+# limits width to 100 columns
+tokei . -e tests/ -e \*.md -e \*.toml -e \*.sh -e \*.svg -f -s code -c 100
